5e975acc2642a0cae5f36db54481b2f5cbf5036c
Ice Lake <naufalshodiq3468@gmail.com>
2024-07-09 21:07:30 +0700

    Open Browser
    Thank you Visiting My Repositories.

---
+++ source.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("test").innerHTML = "Sedang Dialihkan... Mohon Tunggu Sebentar."
+
+<open(URL="https://youtu.be/vCIpDmRvBcM?si=Weixeum494Qcj6fF")
+}
